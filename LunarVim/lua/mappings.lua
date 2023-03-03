7bbf034e6d03297c04f5cc06b9300e17d29853e9
Dzemal <dzemal.memic@htecgroup.com>
2023-03-03 16:01:36 +0100

    2023/03/03, Update lunarvim options.

--- LunarVim/lua/mappings.lua
+++ LunarVim/lua/mappings.lua
@@ -1,7 +1,6 @@
 local map = vim.api.nvim_set_keymap
 
-default_opts= {noremap = true, silent = true}
-expr_options = {noremap = true, expr = true, silent = true}
+local default_opts= {noremap = true, silent = true}
 
 map("v", "<", "<gv", default_opts)
 map("v", ">", ">gv", default_opts)
@@ -14,10 +13,6 @@ map("n", "<esc><bs>", ":bprevious<CR>", default_opts)
 --map('', '<down>', '<nop>', { noremap = true })
 --map('', '<left>', '<nop>', { noremap = true })
 --map('', '<right>', '<nop>', { noremap = true })
---map('n', '<c-j>', '<c-w>j',default_opts)
---map('n', '<c-h>', '<c-w>h',default_opts)
---map('n', '<c-k>', '<c-w>k',default_opts)
---map('n', '<c-l>', '<c-w>l',default_opts)
 map('v', '<C-y>',':* yank +<CR>',{})
 map('v', '<C-Space>',':norm i// <CR>',default_opts)
 map('n', '<C-Space>',':norm i// <CR>',default_opts)
@@ -37,7 +32,3 @@ map('n','gt','<cmd>lua vim.lsp.buf.type_definition()<CR>',default_opts)
 map('n','<C-p>', ':Telescope find_files<CR>',default_opts)
 map('n','<C-g>', ':Telescope grep_string<CR>',default_opts)
 map('n','<C-b>', ':Telescope buffers<CR>',default_opts)
--- Buffer clean
-map('n', '<C-e>', ':BDelete! hidden<CR>',default_opts)
-map('n', '<C-t>', ':BDelete! this<CR>',default_opts)
-map('n', 'gb', ':Gitsigns blame_line<CR>',default_opts)
